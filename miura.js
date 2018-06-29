10bae16d3f4540ec04d2e815910a6a30b6fef76d
Alberto Di Cagno <albertodicagno@gmail.com>
2018-06-29 08:46:56 +0200

    Second commit

--- miura.js
+++ miura.js
@@ -1,11 +1,30 @@
+const xml2js = require('xml2js')
+const http = require('http')
+const parser = new xml2js.Parser()
+
 module.exports = function (RED) {
-    function EtherCATNode(config) {
+    function MiuraNode(config) {
         RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', function (msg) {
-            msg.payload = msg.payload.toLowerCase();
-            node.send(msg);
+        node.on('input', function (msg_in) {
+            if (msg_in.payload && msg_in.payload != {}) {
+                let msg = {}
+                let command = msg_in.command
+                switch (command) {
+                    case 'getStatus':
+                        http.get({
+                            hostname: '192.168.5.250',
+                            port: 5400,
+                            path: '/status.xml',
+                            agent: false  // create a new agent just for this one request
+                        }, (res) => {
+                            msg.payload = msg.payload.toLowerCase()
+                            node.send(res);
+                        });
+                        break;
+                }
+            }
         });
     }
-    RED.nodes.registerType("ethercat", EtherCATNode);
+    RED.nodes.registerType("miura", MiuraNode);
 }
