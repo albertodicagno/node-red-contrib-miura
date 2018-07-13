46cf041006f363b4f2f33a8023a7e91980ce67a9
Alberto Di Cagno <albertodicagno@gmail.com>
2018-07-13 18:53:55 +0200

    Prima versione

--- miura.js
+++ miura.js
@@ -1,11 +1,11 @@
 const xml2js = require('xml2js')
 const request = require('request')
-const parser = new xml2js.Parser()
+const xmlParser = new xml2js.Parser()
 
 module.exports = function (RED) {
     function MiuraNode(config) {
-        RED.nodes.createNode(this, config);
-        var node = this;
+        RED.nodes.createNode(this, config)
+        var node = this
         node.on('input', function (msg_in) {
             if (msg_in.payload && msg_in.payload.command) {
                 let msg = {}
@@ -22,31 +22,37 @@ module.exports = function (RED) {
                         }, function (err, res) {
                             if (!err) {
                                 if (res.statusCode == 200) {
-                                    node.status({ fill: "green", shape: "dot", text: "Richiesta completata." });
-                                    msg.payload = res.body
-                                    node.send(msg)
-                                } else if (res.statusCode == 401) {
-                                    node.status({ fill: "red", shape: "ring", text: "Nome utente o pin errato." });
+                                    node.status({ fill: "green", shape: "dot", text: "Richiesta completata." })
+                                    xmlParser.parseString(res.body, function (err, result) {
+                                        if (err) {
+                                            node.status({ fill: "red", shape: "ring", text: "Errore di sintassi." })
+                                        } else {
+                                            node.send(result)
+                                        }
+                                    })
+                                } else if (res.statusCode == 401 || res.statusCode == 403) {
+                                    node.status({ fill: "red", shape: "ring", text: "Nome utente o pin errato." })
                                 } else {
-                                    node.status({ fill: "red", shape: "ring", text: "Errore. Codice HTTP " + res.statusCode.toString() + "." });
+                                    node.status({ fill: "red", shape: "ring", text: "Errore. Codice HTTP " + res.statusCode.toString() + "." })
                                 }
                             } else {
                                 node.error("HTTP request error", err);
-                                node.status({ fill: "red", shape: "ring", text: "Errore richiesta." });
+                                node.status({ fill: "red", shape: "ring", text: "Errore richiesta." })
                             }
                         })
-                        break;
+                        break
                     default:
-                        node.error("Command " + ((command.length > 0) ? (command + " ") : "") + "not recognized.");
-                        break;
+                        node.error("Command " + ((command.length > 0) ? (command + " ") : "") + "not recognized.")
+                        break
                 }
             }
-        });
+        })
     }
+
     RED.nodes.registerType("miura", MiuraNode, {
         credentials: {
             username: { type: "text" },
             pincode: { type: "password" }
         }
-    });
+    })
 }
